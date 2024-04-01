01435c0e61795f9411a6c99e9bd0f96412dc7806
AadelAferyad <animevineu@gmail.com>
2024-04-01 02:50:10 +0000

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,5 +4,10 @@ file_line { 'file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
