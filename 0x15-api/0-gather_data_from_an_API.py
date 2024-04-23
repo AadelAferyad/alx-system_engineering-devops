e997b27144401d3e87ae89fb52217182c582e38f
AadelAferyad <animevineu@gmail.com>
2024-04-23 01:52:00 +0100

    not sure

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""display todo list by id"""
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+    from json import loads
+
+    employee_id = argv[1]
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos/"\
+        .format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(employee_id)
+    response_todo = get(todo_url)
+    list_emp = response_todo.json()
+    response_user = get(user_url)
+    EMPLOYEE_NAME = response_user.json()['name']
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = len(list_emp)
+    TASK_TITLE = ""
+    for i in list_emp:
+        if i['completed']:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE += i['title'] + "\n"
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks\
+            {NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}")
+    print(TASK_TITLE, end="")
