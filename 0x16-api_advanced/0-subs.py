b4e9f7631fafb1a4135081dcfa3d6235ab7b9048
AadelAferyad <animevineu@gmail.com>
2024-05-08 00:33:38 +0100

    reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,12 @@ from requests import get
 def number_of_subscribers(subreddit):
     """ send get request and return
     the number of subscribers """
-    url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=1"
-    response = get(url)
-    if response.status_code != 200:
-        return (0)
-    r = response.json()
-    return (r['data']['children'][0]['data']['subreddit_subscribers'])
+    if type(subreddit) not str:
+        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {'User-Agent': 'haruma'}
+    response = get(url, header)
+    if response.status_code == 200:
+        r = response.json()
+        return (response['data']['subscribers'])
+    return (0)
